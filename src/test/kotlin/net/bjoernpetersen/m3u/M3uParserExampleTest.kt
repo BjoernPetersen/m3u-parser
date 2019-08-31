6497d1a561e9d310b0c726f86af7a1dd68f2ed25
Björn Petersen <pheasn@gmail.com>
2019-08-31 14:40:50 +0200

    Add even more test files

--- src/test/kotlin/net/bjoernpetersen/m3u/M3uParserExampleTest.kt
+++ src/test/kotlin/net/bjoernpetersen/m3u/M3uParserExampleTest.kt
@@ -24,58 +24,75 @@ class M3uParserExampleTest {
             }
     }
 
-    @Test
-    fun testWikiSimple() {
-        assertThat(M3uParser.parse(javaClass.getResourceAsStream("wiki_simple.m3u").reader()))
-            .hasSize(7)
-            .allMatch { it.duration == null && it.title == null }
-            .matches { list ->
-                val titles = list.asSequence().map { it.location }.distinct().count()
-                titles == list.size
+    @TestFactory
+    fun testWikiSimple(): List<DynamicTest> {
+        return listOf(
+            "wiki_simple.m3u",
+            "wiki_simple_comments.m3u"
+        ).map { name ->
+            dynamicTest(name) {
+                assertThat(M3uParser.parse(javaClass.getResourceAsStream(name).reader()))
+                    .hasSize(7)
+                    .allMatch { it.duration == null && it.title == null }
+                    .matches { list ->
+                        val titles = list.asSequence().map { it.location }.distinct().count()
+                        titles == list.size
+                    }
             }
+        }
     }
 
     @TestFactory
     fun testWikiExtended(): List<DynamicTest> {
-        return listOf("wiki_extended.m3u", "wiki_extended_missing_header.m3u")
-            .map { name ->
-                dynamicTest(name) {
-                    assertThat(M3uParser.parse(javaClass.getResourceAsStream(name).reader()))
-                        .hasSize(7)
-                        .allMatch { it.duration != null && !it.title.isNullOrBlank() }
-                        .matches { list ->
-                            val titles = list.asSequence().map { it.duration }.distinct().count()
-                            titles == list.size
-                        }
-                        .matches { list ->
-                            val titles = list.asSequence().map { it.title }.distinct().count()
-                            titles == list.size
-                        }
-                        .matches { list ->
-                            val titles = list.asSequence().map { it.location }.distinct().count()
-                            titles == list.size
-                        }
-                }
+        return listOf(
+            "wiki_extended.m3u",
+            "wiki_extended_missing_header.m3u",
+            "wiki_extended_comments.m3u"
+        ).map { name ->
+            dynamicTest(name) {
+                assertThat(M3uParser.parse(javaClass.getResourceAsStream(name).reader()))
+                    .hasSize(7)
+                    .allMatch { it.duration != null && !it.title.isNullOrBlank() }
+                    .matches { list ->
+                        val titles = list.asSequence().map { it.duration }.distinct().count()
+                        titles == list.size
+                    }
+                    .matches { list ->
+                        val titles = list.asSequence().map { it.title }.distinct().count()
+                        titles == list.size
+                    }
+                    .matches { list ->
+                        val titles = list.asSequence().map { it.location }.distinct().count()
+                        titles == list.size
+                    }
             }
+        }
     }
 
-    @Test
-    fun testWikiMixed() {
-        val entries = M3uParser.parse(javaClass.getResourceAsStream("wiki_mixed.m3u").reader())
-        assertEquals(7, entries.size)
+    @TestFactory
+    fun testWikiMixed(): List<DynamicTest> {
+        return listOf(
+            "wiki_mixed.m3u",
+            "wiki_mixed_empty_lines.m3u"
+        ).map { name ->
+            dynamicTest(name) {
+                val entries = M3uParser.parse(javaClass.getResourceAsStream(name).reader())
+                assertEquals(7, entries.size)
 
-        val simple = listOf(entries[1], entries[4])
-        simple.forEach {
-            assertThat(it)
-                .returns(null, M3uEntry::duration)
-                .returns(null, M3uEntry::title)
-        }
+                val simple = listOf(entries[1], entries[4])
+                simple.forEach {
+                    assertThat(it)
+                        .returns(null, M3uEntry::duration)
+                        .returns(null, M3uEntry::title)
+                }
 
-        val extended = entries.filterNot { it in simple }
-        extended.forEach { entry ->
-            assertThat(entry)
-                .matches { it.duration != null }
-                .matches { it.title != null }
+                val extended = entries.filterNot { it in simple }
+                extended.forEach { entry ->
+                    assertThat(entry)
+                        .matches { it.duration != null }
+                        .matches { it.title != null }
+                }
+            }
         }
     }
 }
