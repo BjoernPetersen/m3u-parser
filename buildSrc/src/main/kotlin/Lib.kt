58280479e0836541cb0e55dd774cc14c82f4acbb
Björn Petersen <bjoern.petersen@cosee.biz>
2021-01-07 10:49:32 +0100

    Update dependencies

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -1,9 +1,9 @@
 object Lib {
     const val KOTLIN = Plugin.KOTLIN
     const val SLF4J = "1.8.0-beta4"
-    const val KOTLIN_LOGGING = "2.0.3"
+    const val KOTLIN_LOGGING = "2.0.4"
 
     const val JUNIT = "5.7.0"
-    const val ASSERT_J = "3.17.2"
-    const val EQUALSVERIFIER = "3.5"
+    const val ASSERT_J = "3.18.1"
+    const val EQUALSVERIFIER = "3.5.1"
 }
